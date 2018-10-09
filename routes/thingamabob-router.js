32a4ecc6650641275630e9991f6676b6d9d94995
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 10:28:27 +0200

    added a POST to thingamabob router

--- routes/thingamabob-router.js
+++ routes/thingamabob-router.js
@@ -2,43 +2,76 @@
 
 const express = require('express');
 const router = express.Router();
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : false }));
+const jsonParser = bodyParser.json();
 
 const {Thingamabob} = require('../src/models/thingamabob');
 
 const {
+    checkFor400,
     get404,
     get500
 } = require('../src/handlers/error-handlers');
 const {
-    get200
+    getSuccess
 } = require('../src/handlers/success-handlers');
 
+// GET requests
+
+// GET all at /api/thingamabobs/
 router.get('/', (req, res) => {
+    const str = 'thingamabobs';
+
     Thingamabob
         .find()
         .exec()
         .then(thinggies => {
             if (!thinggies) {
-                return get404(res, 'thingamabobs');
+                return get404(res, str);
             }
 
-            return get200(res, thinggies, 'thingamabobs');
+            return getSuccess(res, thinggies, str);
         })
-        .catch(err => get500(res, err, `cannot fetch thingamabobs`));
+        .catch(err => get500(res, err, `cannot fetch ${str}`));
 });
 
+// GET specific thingamabob at /api/thingamabobs/:id
 router.get('/:id', (req, res) => {
+    const str = 'thingamabob';
+
     Thingamabob
         .findById(req.params.id)
         .exec()
         .then(thinggy => {
             if (!thinggy) {
-                return get404(res, `thingamabob ${req.param.id}`);
+                return get404(res, `${str} ${req.param.id}`);
             }
 
-            return get200(res, thinggy, 'thingamabob');
+            return getSuccess(res, thinggy, str);
         })
-        .catch(err => get500(res, err, `cannot fetch thinggy`));
+        .catch(err => get500(res, err, `cannot fetch ${str}`));
+});
+
+
+
+// POST requests
+
+// POST a thingamabob at /api/thingamabobs
+router.post('/', jsonParser, (req, res) => {
+    const reqFields = ['awesome_field'],
+        str = 'thingamabob';
+    
+    checkFor400(req, res, reqFields, str);
+
+    Thingamabob
+        .create(req.body)
+        .then(newT => {
+            return getSuccess(res, newT, str);
+        })
+        .catch(err => get500(res, err, `cannot create ${str}`));
 });
 
 module.exports = router;
