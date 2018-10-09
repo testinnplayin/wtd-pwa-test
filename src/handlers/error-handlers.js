ea656878d983f417b5cfc485cf47bc0648ef86d8
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 15:33:22 +0200

    Tweaked error handlers and added a simple 400

--- src/handlers/error-handlers.js
+++ src/handlers/error-handlers.js
@@ -16,14 +16,20 @@ module.exports = {
         };
 
         if (!req.body) {
+            console.error(`---- ERROR: No body in ${str} request ----`);
             return res.status(400).json({ message : `Badly formed ${str}, no request body`});
         } else if (req.body) {
             reqStatus = checkForField(arrOfFields, req.body);
-            
+
             if (!reqStatus.isOk) {
+                console.error(`---- ERROR: Badly-formed request for ${str}, lacks required field ${field} ----`);
                 return res.status(400).json({ message : `Badly formed ${str}, required field ${reqStatus.field}`});
             }
         }
+    },
+    get400 : function (res, str) {
+        console.error(`---- ERROR: Bad request in count for ${str} ----`);
+        return res.status(400).json({ message : `There has been a problem with the count request for ${str}` });
     },
     get404 : function (res, str) {
         console.error(`---- ERROR: Cannot find ${str}`);
