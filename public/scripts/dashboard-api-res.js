156d5b192e2df96a97b3b491b624a7f920c3bd47
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 15:32:47 +0200

    data for use in dashboard API

---
+++ public/scripts/dashboard-api-res.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export const resources = {
+    bAddress : 'http://localhost:3000',
+    api : 'api/dashboard',
+    elements : {
+        dEle : 'dohickies',
+        tEle : 'thingamabobs',
+        wEle : 'whatchamagiggers'
+    },
+    c : 'count'
+};
