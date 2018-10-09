6321021b710901f24f4162a235c477aded782fc3
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 10:27:23 +0200

    changed name of success handler to be more generic

--- src/handlers/success-handlers.js
+++ src/handlers/success-handlers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    get200 : function (res, data, str) {
+    getSuccess : function (res, data, str) {
         return res.status(200).json({ [`${str}`] : data });
     }
 };
