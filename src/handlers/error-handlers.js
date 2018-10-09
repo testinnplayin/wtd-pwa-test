24816bfc1e6dedee572566e8847c4e468c63dc1c
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 10:27:40 +0200

    added checks for producing a 400

--- src/handlers/error-handlers.js
+++ src/handlers/error-handlers.js
@@ -1,7 +1,31 @@
 'use strict';
 
+function checkForField(arrOfFields, reqObj) {
+    arrOfFields.forEach(field => {
+        if (!reqObj.hasOwnProperty(field)) {
+            return { isOk : false, field : field };
+        }
+    });
+}
+
 module.exports = {
-    get404: function (res, str) {
+    checkFor400 : function (req, res, arrOfFields, str) {
+        let reqStatus = {
+            isOk : true,
+            field : null
+        };
+
+        if (!req.body) {
+            return res.status(400).json({ message : `Badly formed ${str}, no request body`});
+        } else if (req.body) {
+            reqStatus = checkForField(arrOfFields, req.body);
+            
+            if (!reqStatus.isOk) {
+                return res.status(400).json({ message : `Badly formed ${str}, required field ${reqStatus.field}`});
+            }
+        }
+    },
+    get404 : function (res, str) {
         console.error(`---- ERROR: Cannot find ${str}`);
         return res.status(404).json({ message : `Cannot find ${str}` });
     },
