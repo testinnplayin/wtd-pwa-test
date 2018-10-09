25883383a6e6b296dc22158fbcc0e9461e176234
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-09 15:33:00 +0200

    Created dashboard router

---
+++ routes/dashboard-router.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const {Thingamabob} = require('../src/models/thingamabob');
+
+const {
+    get400,
+    get500
+} = require('../src/handlers/error-handlers');
+
+function successCase(res, count, type) {
+    return res.status(200).json({ count : count, type : type });
+}
+
+router.get('/thingamabobs/count', (req, res) => {
+    const str = 'thingamabobs';
+
+    Thingamabob
+        .find({})
+        .countDocuments()
+        .exec()
+        .then(count => {
+            if (!count || count === 'null' || count === 'undefined') {
+                return get400(res, str);
+            }
+
+            return successCase(res, count, str);
+        })
+        .catch(err => get500(res, err, `cannot fetch ${str} count`));
+});
+
+module.exports = router;
