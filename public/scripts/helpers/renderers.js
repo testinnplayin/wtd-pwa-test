8be55f2f75100a899bb3f84d2b835e1765752381
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-11 15:15:56 +0200

    View related changes

--- public/scripts/helpers/renderers.js
+++ public/scripts/helpers/renderers.js
@@ -5,5 +5,20 @@ export default {
         nEle.classList.add(`${klass}-${ind}`);
     
         return nEle;
+    },
+    renderListEle(item, i, pKlass) {
+        let li = document.createElement('li');
+        document.querySelector(pKlass).appendChild(li);
+        
+        const newLiKlass = `li-${i}`;
+        li.classList.add(newLiKlass);
+
+        let btn = document.createElement('button');
+        document.querySelector(`.${newLiKlass}`).appendChild(btn);
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('id', item._id);
+        btn.textContent = item.awesome_field;
+
+        return li;
     }
 }
